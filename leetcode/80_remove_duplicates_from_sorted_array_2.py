c4e680c5d5c017d8399ebed44fc5ed765d4d378f
coolcool <coolcool.with.you@gmail.com>
2025-03-07 23:35:25 +0900

    ADD - https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/submissions/1564354043/?envType=study-plan-v2&envId=top-interview-150

---
+++ leetcode/80_remove_duplicates_from_sorted_array_2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        prev_num = nums[0] - 1
+        nums_count = len(nums)
+        appear_count = 0
+
+        for index in range(nums_count):
+            reverse_index = index - nums_count
+            if nums[reverse_index] == prev_num and appear_count >= 2:
+                del nums[reverse_index]
+            elif nums[reverse_index] > prev_num:
+                appear_count = 0
+
+            prev_num = nums[reverse_index]
+            appear_count += 1
+
+        return len(nums)
