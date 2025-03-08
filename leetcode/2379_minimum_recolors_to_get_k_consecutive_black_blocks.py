c951978b0507b93b5203e39de6363bdf428ba806
coolcool <coolcool.with.you@gmail.com>
2025-03-08 13:58:23 +0900

    ADD - https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/submissions/?envType=daily-question&envId=2025-03-08

---
+++ leetcode/2379_minimum_recolors_to_get_k_consecutive_black_blocks.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        colored = []
+        for index in range(len(blocks)):
+            if blocks[index] == 'W':
+                colored.append(0)
+            else:
+                colored.append(1)
+
+        if k == len(blocks):
+            return k - sum(colored)
+
+        maximum_count = 0
+        for index in range(len(blocks) - k + 1):
+            temp_count = sum(colored[index: index + k])
+            if temp_count == k:
+                return 0
+            elif temp_count > maximum_count:
+                maximum_count = temp_count
+
+        return k - maximum_count
+
+
+def main():
+    # define test case
+    blocks = "WWBBBWBBBBBWWBWWWB"
+    k = 16
+
+    # run
+    solution = Solution()
+    result = solution.minimumRecolors(blocks, k)
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
